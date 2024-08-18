7428cdcf559961e05ec96cd9e7ec2e9633a448f3
Ben Pig Chu <benpichu@gmail.com>
2024-08-18 14:38:27 +0800

    Wall collider

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -34,13 +34,37 @@ namespace ScaleSokoban{
         int height=0;
         int width=0;
 
+        bool[,] walls;
+
+        static Vector2Int[] offsets3x3=new Vector2Int[]{
+            new Vector2Int(-1,-1),
+            new Vector2Int(0,-1),
+            new Vector2Int(1,-1),
+            new Vector2Int(-1,0),
+            new Vector2Int(0,0),
+            new Vector2Int(1,0),
+            new Vector2Int(-1,1),
+            new Vector2Int(0,1),
+            new Vector2Int(1,1),
+        };
         Vector3Int CoordToTilemapCoord(int x,int y)=>new Vector3Int(x,-y,0);
         Vector3 CoordToTilemapCoord(float x,float y)=>new Vector3(x,-y,0);
+
+        void setup3x3wall(int x,int y){
+            foreach(var offset in offsets3x3){
+                var wallX=offset.x+x;
+                var wallY=offset.y+y;
+                if(wallX>=0&&wallX<width&&wallY>=0&&wallY<height){
+                    walls[wallY,wallX]=true;
+                }
+            }
+        }
 
         void LoadTextLevel(string level){
             var rows=level.Split("\n").Where(str=>str!="").ToList();
             height=rows.Count;
             width=rows[0].Length;
+            walls=new bool[height,width];
             // setup ground tilemap
             for(int y=0;y<height;y++){
                 for(int x=0;x<width;x++){
@@ -49,6 +73,11 @@ namespace ScaleSokoban{
                     if(c=='#'){
                         Tilemap.SetTile(tileLocation,Wall);
                         Tilemap.SetTransformMatrix(tileLocation,Matrix4x4.Translate(new Vector3(0,0,-1))*Matrix4x4.Scale(new Vector3(3,3,1)));
+                        setup3x3wall(x,y);
+                    }else if(c=='*'){
+                        Tilemap.SetTile(tileLocation,Wall);
+                        Tilemap.SetTransformMatrix(tileLocation,Matrix4x4.Translate(new Vector3(0,0,-1)));
+                        walls[y,x]=true;
                     }else{
                         Tilemap.SetTile(tileLocation,Ground);
                     }
